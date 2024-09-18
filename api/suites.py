b057ca625de54474f4f822fe82513f15c2bd9266
Ordoptimus <46285734+Ordoptimus@users.noreply.github.com>
2024-09-18 04:27:25 +0000

    suite api base

--- api/suites.py
+++ api/suites.py
@@ -8,9 +8,13 @@ def create_suite(name, org, api_key):
     Creates a new suite for the provided org ID
     '''
     api_endpoint = f"https://api.ghostinspector.com/v1/suites/?apiKey={api_key}"
+    payload = {
+        "name": name,
+        "organization": org
+    }
 
     try:
-        response = requests.post(api_endpoint.format(api_key))
+        response = requests.post(api_endpoint.format(api_key), json=payload)
         response_json = response.json()
         suite_id = response_json['data']['_id']
         return suite_id
@@ -25,19 +29,39 @@ def update_suite(suite_id, updates, api_key):
     Updates given suite with the provided configuration
     '''
     api_endpoint = f"https://api.ghostinspector.com/v1/suites/{suite_id}/?apiKey={api_key}"
-
-    # replace payload with API specifications
     payload = {
         "folder": updates.get("folder_id"),
-        "description": updates.get("description"),
+        "details": updates.get("description"),
         "variables": updates.get("variables")
     }
 
     try:
-        response = requests.post(api_endpoint.format(api_key), json=payload)
+        response = requests.post(api_endpoint.format(suite_id, api_key), json=payload)
         response_json = response.json()
-        return suite_id
+        return response_json
     
     except Exception as e:
         print(f"Error creating suite: {e}")
         return e
+    
+    
+def suite_handler():
+    None
+    
+
+def duplicate_test(test_id, api_key):
+    '''
+    Dupicates existing test as template to be passed for updation
+    '''
+    api_endpoint = f"https://api.ghostinspector.com/v1/tests/{test_id}/duplicate/?apiKey={api_key}"
+
+    try:
+        response = requests.post(api_endpoint.format(test_id, api_key))
+        response_json = response.json()
+        test_id = response_json['data']['_id']
+        return test_id
+    
+    except Exception as e:
+        print(f"Error duplicating suite: {e}")
+        return e
+
